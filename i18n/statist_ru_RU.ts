14b914684dd4feb9b0443d58f566f63cf6a25077
Alexander Bruy <alexander.bruy@gmail.com>
2012-12-05 15:20:23 +0200

    add i18n directory with initial strings

---
+++ i18n/statist_ru_RU.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>IdentifyPlus</name>
+    <message>
+        <location filename="identifyplus.py" line="72"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="identifyplus.py" line="72"/>
+        <source>Quantum GIS %1 detected.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="identifyplus.py" line="72"/>
+        <source>This version of IdentifyPlus requires at least QGIS version 1.9.0
+Plugin will not be enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="identifyplus.py" line="101"/>
+        <source>IdentifyPlus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="identifyplus.py" line="83"/>
+        <source>About IdentifyPlus...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
