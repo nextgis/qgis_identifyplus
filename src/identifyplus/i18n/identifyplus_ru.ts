71fed620b101b84f2492c62a2197909638c79f59
Ivan Barsukov <i.barsukov@protonmail.com>
2024-11-19 20:20:31 +0700

    Update translations

--- src/identifyplus/i18n/identifyplus_ru.ts
+++ src/identifyplus/i18n/identifyplus_ru.ts
@@ -4,49 +4,54 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../aboutdialog.py" line="83"/>
+        <location filename="../aboutdialog.py" line="204"/>
         <source>Developers</source>
         <translation>Разработчики</translation>
     </message>
     <message>
-        <location filename="../aboutdialog.py" line="84"/>
+        <location filename="../aboutdialog.py" line="205"/>
         <source>Homepage</source>
         <translation>Домашняя страница</translation>
     </message>
     <message>
-        <location filename="../aboutdialog.py" line="85"/>
+        <location filename="../aboutdialog.py" line="206"/>
         <source>Please report bugs at</source>
         <translation>Пожалуйста, сообщайте о багах в</translation>
     </message>
     <message>
-        <location filename="../aboutdialog.py" line="88"/>
+        <location filename="../aboutdialog.py" line="209"/>
         <source>Video with an overview of the plugin</source>
         <translation>Видео с обзором на плагин</translation>
     </message>
     <message>
-        <location filename="../aboutdialog.py" line="89"/>
+        <location filename="../aboutdialog.py" line="210"/>
         <source>Other helpful services by NextGIS</source>
         <translation>Другие полезные сервисы от NextGIS</translation>
     </message>
     <message>
-        <location filename="../aboutdialog.py" line="90"/>
+        <location filename="../aboutdialog.py" line="211"/>
         <source>Convenient up-to-date data extracts for any place in the world</source>
         <translation>Удобная выборка актуальных данных из любой точки мира</translation>
     </message>
     <message>
-        <location filename="../aboutdialog.py" line="93"/>
+        <location filename="../aboutdialog.py" line="214"/>
         <source>Fully featured Web GIS service</source>
         <translation>Полнофункциональный Веб ГИС-сервис</translation>
     </message>
     <message>
-        <location filename="../aboutdialog.py" line="78"/>
+        <location filename="../aboutdialog.py" line="199"/>
         <source>REPORT_END</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../aboutdialog.py" line="87"/>
+        <location filename="../aboutdialog.py" line="208"/>
         <source>bugtracker</source>
         <translation>багтрекер</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.py" line="61"/>
+        <source> by NextGIS</source>
+        <translation> от NextGIS</translation>
     </message>
 </context>
 <context>
@@ -57,25 +62,64 @@
         <translation>О {plugin_name}</translation>
     </message>
     <message>
-        <location filename="../ui/aboutdialogbase.ui" line="27"/>
-        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+        <location filename="../ui/aboutdialogbase.ui" line="33"/>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="54"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="72"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="94"/>
+        <source>Components</source>
+        <translation>Компоненты</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="116"/>
+        <source>Contributors</source>
+        <translation>Участники</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="158"/>
+        <source>{plugin_name}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="168"/>
+        <source>Version {version}</source>
+        <translation>Версия {version}</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="203"/>
+        <source>Get involved</source>
+        <translation>Присоединяйтесь</translation>
     </message>
 </context>
 <context>
     <name>AttributesTable</name>
     <message>
-        <location filename="../ui_attributestable.py" line="52"/>
+        <location filename="../ui/attributestable.ui" line="14"/>
         <source>Form</source>
         <translation>Результаты определения</translation>
     </message>
     <message>
-        <location filename="../ui_attributestable.py" line="54"/>
+        <location filename="../ui/attributestable.ui" line="63"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="../ui_attributestable.py" line="56"/>
+        <location filename="../ui/attributestable.ui" line="68"/>
         <source>Value</source>
         <translation>Значение</translation>
     </message>
@@ -83,32 +127,32 @@
 <context>
     <name>AttributesTableWithImages</name>
     <message>
-        <location filename="../ui_attributestablewithimages.py" line="90"/>
+        <location filename="../ui/attributestablewithimages.ui" line="20"/>
         <source>Form</source>
         <translation>Результаты определения</translation>
     </message>
     <message>
-        <location filename="../ui_attributestablewithimages.py" line="91"/>
+        <location filename="../ui/attributestablewithimages.ui" line="51"/>
         <source>Attributes</source>
         <translation>Атрибуты</translation>
     </message>
     <message>
-        <location filename="../ui_attributestablewithimages.py" line="97"/>
+        <location filename="../ui/attributestablewithimages.ui" line="64"/>
         <source>Image</source>
         <translation>Изображение</translation>
     </message>
     <message>
-        <location filename="../ui_attributestablewithimages.py" line="93"/>
+        <location filename="../ui/attributestablewithimages.ui" line="126"/>
         <source>Download photos</source>
         <translation>Загрузка изображений</translation>
     </message>
     <message>
-        <location filename="../ui_attributestablewithimages.py" line="95"/>
+        <location filename="../ui/attributestablewithimages.ui" line="146"/>
         <source>Save all photos to disk</source>
         <translation>Сохранить все изображения на диск</translation>
     </message>
     <message>
-        <location filename="../ui_attributestablewithimages.py" line="96"/>
+        <location filename="../ui/attributestablewithimages.ui" line="149"/>
         <source>...</source>
         <translation>...</translation>
     </message>
@@ -170,42 +214,42 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>IdentifyPlus</name>
     <message>
-        <location filename="../identifyplus.py" line="60"/>
+        <location filename="../identifyplus.py" line="61"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
     <message>
-        <location filename="../identifyplus.py" line="60"/>
+        <location filename="../identifyplus.py" line="61"/>
         <source>QGIS {} detected.</source>
         <translation>Обнаружен QGIS {}.</translation>
     </message>
     <message>
-        <location filename="../identifyplus.py" line="60"/>
+        <location filename="../identifyplus.py" line="61"/>
         <source>This version of IdentifyPlus requires at least QGIS version 3.0.</source>
         <translation>Для работы IdentifyPlus минимальной версией QGIS является 3.0.</translation>
     </message>
     <message>
-        <location filename="../identifyplus.py" line="60"/>
+        <location filename="../identifyplus.py" line="61"/>
         <source>Plugin will not be enabled.</source>
         <translation>Плагин не будет запущен.</translation>
     </message>
     <message>
-        <location filename="../identifyplus.py" line="154"/>
+        <location filename="../identifyplus.py" line="164"/>
         <source>IdentifyPlus</source>
         <translation>IdentifyPlus</translation>
     </message>
     <message>
-        <location filename="../identifyplus.py" line="121"/>
+        <location filename="../identifyplus.py" line="122"/>
         <source>Extended identify tool</source>
         <translation>Расширенный инструмент идентификации</translation>
     </message>
     <message>
-        <location filename="../identifyplus.py" line="131"/>
+        <location filename="../identifyplus.py" line="132"/>
         <source>About IdentifyPlus…</source>
         <translation>О плагине IdentifyPlus…</translation>
     </message>
     <message>
-        <location filename="../identifyplus.py" line="137"/>
+        <location filename="../identifyplus.py" line="138"/>
         <source>About IdentifyPlus</source>
         <translation>О плагине IdentifyPlus</translation>
     </message>
@@ -213,32 +257,32 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>IdentifyPlusResults</name>
     <message>
-        <location filename="../ui_identifyplusresultsbase.py" line="127"/>
+        <location filename="../ui/identifyplusresultsbase.ui" line="20"/>
         <source>Form</source>
         <translation>Результаты определения</translation>
     </message>
     <message>
-        <location filename="../ui_identifyplusresultsbase.py" line="128"/>
+        <location filename="../ui/identifyplusresultsbase.ui" line="52"/>
         <source>First feature</source>
         <translation>Первый объект</translation>
     </message>
     <message>
-        <location filename="../ui_identifyplusresultsbase.py" line="130"/>
+        <location filename="../ui/identifyplusresultsbase.ui" line="66"/>
         <source>Previous feature</source>
         <translation>Предыдущий объект</translation>
     </message>
     <message>
-        <location filename="../ui_identifyplusresultsbase.py" line="133"/>
+        <location filename="../ui/identifyplusresultsbase.ui" line="96"/>
         <source>Next feature</source>
         <translation>Следующий объект</translation>
     </message>
     <message>
-        <location filename="../ui_identifyplusresultsbase.py" line="135"/>
+        <location filename="../ui/identifyplusresultsbase.ui" line="110"/>
         <source>Last feature</source>
         <translation>Последний объект</translation>
     </message>
     <message>
-        <location filename="../ui_identifyplusresultsbase.py" line="137"/>
+        <location filename="../ui/identifyplusresultsbase.ui" line="224"/>
         <source>Identification status</source>
         <translation>Статус идентификацмм</translation>
     </message>
@@ -258,12 +302,12 @@ p, li { white-space: pre-wrap; }
         <translation>Объект %s из %s (%s)</translation>
     </message>
     <message>
-        <location filename="../ui_identifyplusresultsbase.py" line="136"/>
+        <location filename="../ui/identifyplusresultsbase.ui" line="113"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="../ui_identifyplusresultsbase.py" line="132"/>
+        <location filename="../ui/identifyplusresultsbase.ui" line="86"/>
         <source>Feature X from Y (layer)</source>
         <translation>Объект X из Y (layer)</translation>
     </message>
@@ -279,12 +323,12 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>Image</name>
     <message>
-        <location filename="../identifytool_ngw.py" line="424"/>
+        <location filename="../identifytool_ngw.py" line="426"/>
         <source>Download photo</source>
         <translation>Загрузка фото</translation>
     </message>
     <message>
-        <location filename="../identifytool_ngw.py" line="436"/>
+        <location filename="../identifytool_ngw.py" line="438"/>
         <source>Delete photo</source>
         <translation>Удаление фото</translation>
     </message>
@@ -292,7 +336,7 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>ImageDownloadDialog</name>
     <message>
-        <location filename="../identifytool_ngw.py" line="703"/>
+        <location filename="../identifytool_ngw.py" line="705"/>
         <source>Download images process</source>
         <translation>Процесс загрузки изображений</translation>
     </message>
@@ -300,7 +344,7 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>ImageLabel</name>
     <message>
-        <location filename="../identifytool_ngw.py" line="361"/>
+        <location filename="../identifytool_ngw.py" line="363"/>
         <source>Loading...</source>
         <translation>Загрузка...</translation>
     </message>
@@ -308,7 +352,7 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>ImageUploadDialog</name>
     <message>
-        <location filename="../identifytool_ngw.py" line="685"/>
+        <location filename="../identifytool_ngw.py" line="687"/>
         <source>Upload images process</source>
         <translation>Загрузка изображений на сервер</translation>
     </message>
@@ -316,42 +360,42 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>NGWImagesView</name>
     <message>
-        <location filename="../identifytool_ngw.py" line="526"/>
+        <location filename="../identifytool_ngw.py" line="528"/>
         <source>Download photos</source>
         <translation>Загрузка изображений</translation>
     </message>
     <message>
-        <location filename="../identifytool_ngw.py" line="536"/>
+        <location filename="../identifytool_ngw.py" line="538"/>
         <source>Add photo(s)</source>
         <translation>Добавление изображений</translation>
     </message>
     <message>
-        <location filename="../identifytool_ngw.py" line="559"/>
+        <location filename="../identifytool_ngw.py" line="561"/>
         <source>Loading...</source>
         <translation>Загрузка...</translation>
     </message>
     <message>
-        <location filename="../identifytool_ngw.py" line="572"/>
+        <location filename="../identifytool_ngw.py" line="574"/>
         <source>No photos</source>
         <translation>Нет фото</translation>
     </message>
     <message>
-        <location filename="../identifytool_ngw.py" line="616"/>
+        <location filename="../identifytool_ngw.py" line="618"/>
         <source>Choose photo(s)</source>
         <translation>Выбор фото</translation>
     </message>
     <message>
-        <location filename="../identifytool_ngw.py" line="616"/>
+        <location filename="../identifytool_ngw.py" line="618"/>
         <source>Image Files (*.png *.jpg *.bmp)</source>
         <translation>Файлы изображений (*.png *.jpg *.bmp)</translation>
     </message>
     <message>
-        <location filename="../identifytool_ngw.py" line="639"/>
+        <location filename="../identifytool_ngw.py" line="641"/>
         <source>Save photo</source>
         <translation>Сохранение фото</translation>
     </message>
     <message>
-        <location filename="../identifytool_ngw.py" line="663"/>
+        <location filename="../identifytool_ngw.py" line="665"/>
         <source>Select directory fo save photos</source>
         <translation>Выбор директории для сохранения изображений</translation>
     </message>
@@ -359,7 +403,7 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>NGWTool</name>
     <message>
-        <location filename="../identifytool_ngw.py" line="102"/>
+        <location filename="../identifytool_ngw.py" line="103"/>
         <source>Photos (ngw)</source>
         <translation>Фото (ngw)</translation>
     </message>
